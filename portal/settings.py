77c684d8abde0158c5a6975ac538d519f38bb6a7
Shashank Sharma <shashank.sharma98@gmail.com>
2018-09-20 14:45:42 +0530

    modify allowed host

--- portal/settings.py
+++ portal/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'soh-cyber.herokuapp.com']
 
 
 # Application definition
